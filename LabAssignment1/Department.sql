feb13140d6b9cd92aaf4b76ad76f240ae4c4012b
Krittika-Mondal-06 <krittika.mon606@gmail.com>
2025-01-16 20:18:04 +0530

    Another Code

---
+++ LabAssignment1/Department.sql
@@ -0,0 +1,11 @@
+CREATE TABLE Departments (
+DepartmentID INT PRIMARY KEY,
+DepartmentName VARCHAR(50) NOT NULL
+);
+INSERT INTO Departments (DepartmentID, DepartmentName)
+VALUES
+
+(1, 'HR'),
+(2, 'IT'),
+(3, 'Finance'),
+(4, 'Marketing');
